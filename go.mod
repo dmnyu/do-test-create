78d5abbe2bcd55f4606e8cf83c7224939203ab6a
Don Mennerich <don.mennerich@nyu.edu>
2024-02-23 08:33:47 -0500

    updating code

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/dmnyu/do-test-create
 go 1.21.6
 
 require (
-	github.com/nyudlts/go-aspace v0.5.1 // indirect
-	gopkg.in/yaml.v2 v2.3.0 // indirect
+	github.com/google/uuid v1.6.0
+	github.com/nyudlts/go-aspace v0.5.1
 )
+
+require gopkg.in/yaml.v2 v2.3.0 // indirect
--- main.go
+++ main.go
@@ -3,41 +3,24 @@ package main
 import (
 	"fmt"
 
+	"github.com/google/uuid"
 	"github.com/nyudlts/go-aspace"
-)
-
-var (
-	client *aspace.ASClient
-	ao     aspace.ArchivalObject
-	repoID = 6
-	aoID   = 980443
 )
 
 func main() {
 	var err error
-	client, err = aspace.NewClient("go-aspace.yml", "dev", 20)
+	client, err := aspace.NewClient("go-aspace.yml", "dev", 20)
 	if err != nil {
 		panic(err)
 	}
 
-	ao, err = client.GetArchivalObject(6, aoID)
-	if err != nil {
-		panic(err)
-	}
-
-	createDO()
-
-}
-
-func createDO() {
-
-	fmt.Println(ao)
-
+	//create the do
 	do := new(aspace.DigitalObject)
-	do.Title = ao.Title
-	do.DigitalObjectID = ao.ComponentId
+	do.Title = "DO TEST OBJECT"
+	do.DigitalObjectID = uuid.New().String()
 	do.Publish = true
 
+	//create file version 1
 	fv1 := new(aspace.FileVersion)
 	fv1.FileURI = "https://hdl.handle.net/2333.1/material-request-placeholder"
 	fv1.Publish = true
@@ -45,17 +28,22 @@ func createDO() {
 	fv1.XLinkActuateAttribute = "onLoad"
 	fv1.XLinkShowAttribute = "new"
 
+	//create fileversion 2
 	fv2 := new(aspace.FileVersion)
 	fv2.FileURI = "https://rstar.url.com"
 	fv2.Publish = false
 	fv2.UseStatement = "electronic-records-master"
 
+	//append new file versions to do fv slice
 	do.FileVersions = append(do.FileVersions, *fv1, *fv2)
 
+	//make the request
 	responseBody, err := client.CreateDigitalObject(6, *do)
 	if err != nil {
 		panic(err)
 	}
 
+	//print the request
 	fmt.Println(responseBody)
+
 }
